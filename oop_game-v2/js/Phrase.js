87f8e52e3ff169e5ea071f4a1074b1c38bb9a3a3
Zach Smith <rysskismith@gmail.com>
2020-06-22 13:53:59 -0500

    working step 6

--- oop_game-v2/js/Phrase.js
+++ oop_game-v2/js/Phrase.js
@@ -7,8 +7,20 @@ class Phrase {
 * Display phrase on game board
 */
 addPhraseToDisplay() {
+const characters = Game.phrases.split("")
+const regex = new RegExp(/[a-z]/i)
+for (let i = 0; i < characters; i++) {
+    if (regex.test(characters[i])) {
+        let li = document.createElement('li')
+        li.className = `hide letter ${[i]}`
+    }
+    
+// loop over the characters
+// if it is a letter, create class based on the letter and append
+// else create a space class and append 
 
 };
 
 
+}
 }
