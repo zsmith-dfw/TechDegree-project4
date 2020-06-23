710b65f61d86f25b410fd6381708813fbb12514d
Zach Smith <rysskismith@gmail.com>
2020-06-22 21:33:48 -0500

    still on step 6

--- oop_game-v2/js/Phrase.js
+++ oop_game-v2/js/Phrase.js
@@ -7,30 +7,28 @@ class Phrase {
 * Display phrase on game board
 */
 addPhraseToDisplay() {
-const phraseId = document.getElementById('phrase')
+const phraseId = document.querySelector('#phrase') // probably don't need this anymore
+const listId = document.querySelector('ul')
+listId.className ='letters'
+console.log(listId)
 const characters = this.phrase.split("")
 console.log(characters)
 const regex = new RegExp(/[a-z]/i)
-for (let i = 0; i < characters; i++) {
-    if (regex.test(characters[i])) {
-        let li = document.createElement('li')
-        li.className = `hide letter ${characters[i]}`
-        phraseId.appendChild(li)
-        
-    } else {
-        let li2 = document.createElement('li')
-        li2.className = 'space'
-        phraseId.appendChild(li2)
+    for (let i = 0; i < characters; i++) {
+            if (regex.test(characters[i])) {
+                let li = document.createElement('li')
+                li.className = `hide letter ${characters[i]}`
+                listId.appendChild(li)
+                
+            } else {
+                let li2 = document.createElement('li')
+                li2.className = 'space'
+                listId.appendChild(li2)
+            }
+        }
     }
-
-    
+}
+// psuedocode   
 // loop over the characters
 // if it is a letter, create class based on the letter and append
 // else create a space class and append 
-
-};
-
-
-}
-}
-
